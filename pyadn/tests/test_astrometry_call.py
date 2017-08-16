66502a9e849a5de395ac11f2c0e1b7a7f070f71f
gitj <glenn.caltech@gmail.com>
2017-08-16 10:43:06 -0400

    check path

--- pyadn/tests/test_astrometry_call.py
+++ pyadn/tests/test_astrometry_call.py
@@ -1,5 +1,6 @@
-import os
+from subprocess import check_output
 
 def test_call_astrometry():
-    os.system("solve-field")
-    os.system("wcsinfo")
+    print check_output("echo $PATH",shell=True)
+    print check_output("$HOME/an/bin/solve-field",shell=True)
+    print check_output("solve-field",shell=True)
